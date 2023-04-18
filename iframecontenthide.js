37b909236146d9bddab1b70e26265038350c3dd4
Christopher Wilkinson <chiwilkinson@gmail.com>
2023-04-18 22:23:21 +0100

    Update iframecontenthide.js

--- iframecontenthide.js
+++ iframecontenthide.js
@@ -1,6 +1,6 @@
       // masthead, colophon
       if ( window.location !== window.parent.location ) {
-  // The page is in an iframe, hide specified element
-        //document.write("in iframe");
-        //document.getElementById("mycontent").classList.add('notinframe');
-      document.getElementById("mycontent").style.cssText += "display: none;";
+            // The page is in an iframe, hide specified element
+            document.getElementById("masthead").style.cssText += "display: none;";
+            document.getElementById("colophon").style.cssText += "display: none;";    
+      }
